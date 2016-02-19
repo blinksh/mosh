a10c5f5d7521982148c1035b7a9d4c66776c9834
Carlos Cabanero <cc@blink.sh>
2016-02-19 10:02:57 -0500

    Build Script with support to all platforms

--- build-ios.sh
+++ build-ios.sh
@@ -65,23 +65,23 @@ buildit()
 
 findLatestSDKVersion iPhoneOS
 
-#buildit armv7 iPhoneOS
-#buildit armv7s iPhoneOS
-#buildit arm64 iPhoneOS
+buildit armv7 iPhoneOS
+buildit armv7s iPhoneOS
+buildit arm64 iPhoneOS
 buildit i386 iPhoneSimulator
 buildit x86_64 iPhoneSimulator
 
 LIPO=$(xcrun -sdk iphoneos -find lipo)
-$LIPO -create $pwd/output/x86_64/libmoshcrypto.a $pwd/output/i386/libmoshcrypto.a -output $pwd/output/libmoshcrypto.a
+$LIPO -create $pwd/output/x86_64/libmoshcrypto.a $pwd/output/i386/libmoshcrypto.a $pwd/output/armv7/libmoshcrypto.a $pwd/output/armv7s/libmoshcrypto.a $pwd/output/arm64/libmoshcrypto.a  -output $pwd/output/libmoshcrypto.a
 LIPO=$(xcrun -sdk iphoneos -find lipo)
-$LIPO -create $pwd/output/x86_64/libmoshnetwork.a $pwd/output/i386/libmoshnetwork.a -output $pwd/output/libmoshnetwork.a
+$LIPO -create $pwd/output/x86_64/libmoshnetwork.a $pwd/output/i386/libmoshnetwork.a -output $pwd/output/libmoshnetwork.a $pwd/output/armv7/libmoshnetwork.a $pwd/output/armv7s/libmoshnetwork.a $pwd/output/arm64/libmoshnetwork.a 
 LIPO=$(xcrun -sdk iphoneos -find lipo)
-$LIPO -create $pwd/output/x86_64/libmoshprotos.a $pwd/output/i386/libmoshprotos.a -output $pwd/output/libmoshprotos.a
+$LIPO -create $pwd/output/x86_64/libmoshprotos.a $pwd/output/i386/libmoshprotos.a -output $pwd/output/libmoshprotos.a $pwd/output/armv7/libmoshprotos.a $pwd/output/armv7s/libmoshprotos.a $pwd/output/arm64/libmoshprotos.a 
 LIPO=$(xcrun -sdk iphoneos -find lipo)
-$LIPO -create $pwd/output/x86_64/libmoshios.a $pwd/output/i386/libmoshios.a -output $pwd/output/libmoshios.a
+$LIPO -create $pwd/output/x86_64/libmoshios.a $pwd/output/i386/libmoshios.a -output $pwd/output/libmoshios.a $pwd/output/armv7/libmoshios.a $pwd/output/armv7s/libmoshios.a $pwd/output/arm64/libmoshios.a 
 LIPO=$(xcrun -sdk iphoneos -find lipo)
-$LIPO -create $pwd/output/x86_64/libmoshstatesync.a $pwd/output/i386/libmoshstatesync.a -output $pwd/output/libmoshstatesync.a
+$LIPO -create $pwd/output/x86_64/libmoshstatesync.a $pwd/output/i386/libmoshstatesync.a -output $pwd/output/libmoshstatesync.a $pwd/output/armv7/libmoshstatesync.a $pwd/output/armv7s/libmoshstatesync.a $pwd/output/arm64/libmoshstatesync.a 
 LIPO=$(xcrun -sdk iphoneos -find lipo)
-$LIPO -create $pwd/output/x86_64/libmoshterminal.a $pwd/output/i386/libmoshterminal.a -output $pwd/output/libmoshterminal.a
+$LIPO -create $pwd/output/x86_64/libmoshterminal.a $pwd/output/i386/libmoshterminal.a -output $pwd/output/libmoshterminal.a $pwd/output/armv7/libmoshterminal.a $pwd/output/armv7s/libmoshterminal.a $pwd/output/arm64/libmoshterminal.a 
 LIPO=$(xcrun -sdk iphoneos -find lipo)
-$LIPO -create $pwd/output/x86_64/libmoshutil.a $pwd/output/i386/libmoshutil.a -output $pwd/output/libmoshutil.a
+$LIPO -create $pwd/output/x86_64/libmoshutil.a $pwd/output/i386/libmoshutil.a -output $pwd/output/libmoshutil.a $pwd/output/armv7/libmoshutil.a $pwd/output/armv7s/libmoshutil.a $pwd/output/arm64/libmoshutil.a 
