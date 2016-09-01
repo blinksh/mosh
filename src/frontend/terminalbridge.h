5edab65aa3f94e8edf27011e5c47324c2334409f
Daria Phoebe Brashear <dariaphoebe@auristor.com>
2016-09-01 14:10:50 -0500

    make terminalbridge.h compilable as-is in objC
    missing header for FILE, and a missing ; mean this won't work

--- src/frontend/terminalbridge.h
+++ src/frontend/terminalbridge.h
@@ -2,8 +2,10 @@
 #define TERMINALBRIDGE_H
 
 #include <sys/ioctl.h>
+#include <sys/file.h>
+#include <stdio.h>
 
 int mosh_main(FILE *f_in, FILE *f_out, struct winsize *window_size,
-	      const char *ip, const char *port, const char *key, const char *predict_mode)
+	      const char *ip, const char *port, const char *key, const char *predict_mode);
 
 #endif
