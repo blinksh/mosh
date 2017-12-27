3871d8f5b9a3a4805508e378f50733f505c28f70
Yury Korolev <yury.korolev@gmail.com>
2017-12-27 22:18:59 +0300

    Renew closes sockets

--- src/network/network.cc
+++ src/network/network.cc
@@ -451,6 +451,10 @@ string Connection::recv( void )
 	   || (e.the_errno == EWOULDBLOCK) ) {
 	assert( !islast );
 	continue;
+      } else if (e.the_errno == ENOTCONN) {
+        hop_port();
+        socks.erase(it--);
+        continue;
       } else {
 	throw;
       }
